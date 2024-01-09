7acda4b6d1448b53a0aa0c8896f9dfdaef8ab48d
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-01-09 05:20:55 -0800

    printing alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-lastdig = abs(number) % 10
-if number < 0:
-    lastdig = -(lastdig)
-thestring = "Last digit of {} is {}".format(number, lastdig)
-if lastdig > 5:
-    print(f"{thestring} and is greater than 5")
-elif lastdig == 0:
-    print(f"{thestring} and is 0")
-elif lastdig < 6:
-    print(f"{thestring} and is less than 6 and not 0")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end='')
