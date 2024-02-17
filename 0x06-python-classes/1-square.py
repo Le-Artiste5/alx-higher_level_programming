7ad45c0c29a11a7c53e42d22d74008bf92ad97ce
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-02-17 03:36:45 -0800

    Defining a class called square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+class Square:
+    """Represents a class called Square"""
+
+    def __init__(self, size):
+        """Initializes a size
+
+        Args:
+            size: The size of the square.
+            """
+        self.__size = size
