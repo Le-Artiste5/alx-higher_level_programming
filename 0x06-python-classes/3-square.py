eaf3a52e5bb6a0137728a9b30f798a4bf1f07c02
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-02-18 08:08:33 -0800

    Returning the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Defining a class Square"""
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize the new square
+
+        Args:
+            size: The new size of the square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance method that returns the current square"""
+        return(self.__size * self.__size)
