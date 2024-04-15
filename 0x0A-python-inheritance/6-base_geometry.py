f9520d31ed164fc1dff7091566e681d3beafceb5
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-15 14:37:43 -0700

    This adds a method to a parent class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""This task develops from the previous cklass"""
+
+class BaseGeometry:
+    """This class is the parent class"""
+
+    def area(self):
+        """This particular function doesnt do much right now"""
+        raise Exception("area() is not implemented")
+
