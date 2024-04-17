8a9c90f045923369fd98d74adaaa3b496b8c6e85
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-17 07:21:58 -0700

    appending a text to a file and writing if file already exists

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""This module appends a string to an already existing content"""
+
+def append_write(filename="", text=""):
+    """This function appends to a file
+
+    Args:
+        filename(str): This is the name of the file
+        text(str): the text to be added to the end of the file
+
+    Return:
+        This returns the number of characters
+    """
+
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
