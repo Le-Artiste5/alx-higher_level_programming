8bfadeffb20f4533d2832684742dc3d85ca2e3bc
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-17 07:11:26 -0700

    Writing into a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,13 +3,12 @@
 
 def write_file(filename="", text=""):
     """This function writes a string and counts the number of charcters in it
-
     Args:
-        filename: The file to be written into
-        text: the text in the file
+        filename(str): The file to be written into
+        text(str): the text in the file
 
-    Return: returns the number of characters in the file
+    Return: 
+        returns the number of characters in the file
     """
-
-    with open(filename, "r+", encoding="utf-8") as f:
-        return(f.write(text))
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
