44f6cb1df94473b80e297953e236a4df15ab0d6e
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-17 11:42:48 -0700

    list of dictionaries

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module returns a data structure with dictionaries"""
+
+def class_to_json(obj):
+    """Data structures to be listed by this function"""
+
+    return obj.__dic__
