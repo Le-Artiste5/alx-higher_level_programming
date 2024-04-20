d349cdca48d6ae11abe2cc35ba62063ec2e0d045
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-20 06:41:25 -0700

    This inherits from a class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,23 @@ class MyInt(int):
     """This class inverts operators"""
 
     def _eqq__(self, value):
-        """overrides the equal operator"""
+        """overrides the equal operator
+
+        Args:
+            value: This is the value to be checked
+
+        Return:
+            Returns the inverted sign
+        """
         return self.real != value
 
     def __neqq__(self, value):
-        """Overrides the not equal to operator"""
+        """Overrides the not equal to operator
+
+        Args:
+            value: This is the value to be checked
+
+        Return:
+            Returns the inverted sign
+        """
         return self.real == value
