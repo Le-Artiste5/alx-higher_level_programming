97633ca4ebf689a0ff14bd280a13d3550423aa12
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-20 07:39:35 -0700

    Creating a class and returning an attribute

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a class Student in the class."""
+
+
+class Student:
+    """Represent a student in a class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student class
+
+        Args:
+            first_name: The first name of the student.
+            last_name: The last name of the student.
+            age: The age of the student.
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {b: getattr(self, b) for b in attrs if hasattr(self, b)}
+        return self.__dict__
