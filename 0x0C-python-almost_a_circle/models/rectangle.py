802a85ae7dffb8ad57e1d3a7a13227ad08c4659d
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-26 05:34:32 -0700

    Assigning the private instance attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This projects helps the creation of a new class"""
-from models.base import Base
+from base import Base
 
 class Rectangle(Base):
     """This class inherits from a parent class called base"""
@@ -27,3 +27,35 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+        @property
+        def width(self):
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            self.__width = value
+
+        @property
+        def height(self):
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            self.__height = value
+        
+        @property
+        def x(self):
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            self.__x = value
+
+        @property
+        def y(self):
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            self.__y = value
