2896f9a5ce58c6b035759c4cf6589191e9de2a08
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-04-26 21:38:42 -0700

    Ä square and its properties

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""""This project deals with a new class called square"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """This is a class that inherits from rectangle"""
+
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """"This a function that holds the arguments of the square
+
+        Args:
+            size: This is the size of the square
+            x: This is the horizontal
+            y: This is the vertical of the square
+            id: This is the id
+        """
+
+        super().__init___(size, size, x, y, id )
+
+        @property
+        def size(self):
+            """This is the size of the square"""
+            return self.width
+
+        @size.setter
+        def size(self, value):
+            self.width = value
+            self.height = value
+
