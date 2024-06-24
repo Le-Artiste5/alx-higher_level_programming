6ead193abd9c601cca965e4448c04a7b2dcb9dae
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-06-23 19:34:52 -0700

    Using loops in js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* a script that prints three lines using loop */
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
