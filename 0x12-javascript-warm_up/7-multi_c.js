c11d01a337f208a098376de2629d71e09e82b62f
Le-Artiste5 <johnsonotobong2@gmail.com>
2024-06-23 19:46:42 -0700

    looping a number of arguments

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* a script that prints x a number of times */
+
+const x = process.argv[2];
+
+if (x === undefined || isNaN(parseInt(x))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(x); i++) {
+    console.log('C is fun');
+  }
+}
